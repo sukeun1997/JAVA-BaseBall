20e888d781e8abac3e938da8aea7a23531ab3c80
sukeun <busan-sy@naver.com>
2022-05-31 22:54:48 +0900

    FEAT : 2,3번째 자리 볼, 스트라이크 TEST : 2,3번째 자리 볼, 스트라이크 CLN : 테스트코드 리팩토링

--- src/test/java/basballRe/baseball/BallTest.java
+++ src/test/java/basballRe/baseball/BallTest.java
@@ -1,6 +1,5 @@
 package basballRe.baseball;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,26 +17,56 @@ public class BallTest {
     @DisplayName("1번째 자리 1 볼")
     void oneBall() {
         player = new Balls(List.of(3, 0, 0));
-        baseBall.judgeBallOfIndex(com, player, 0);
-        assertThat(baseBall.getBall()).isEqualTo(1);
+        isBallOfIndex(0);
     }
 
     @Test
     @DisplayName("1번째 자리 1 스트라이크")
     void oneStrike() {
         player = new Balls(List.of(1, 0, 0));
-        baseBall.judgeBallOfIndex(com, player, 0);
-        assertThat(baseBall.getStrike()).isEqualTo(1);
-        assertThat(baseBall.getBall()).isEqualTo(0);
+        isStrikeOfIndex(0);
+    }
+
+
+    @Test
+    @DisplayName("2번째 자리 볼")
+    void oneBall_2() {
+        player = new Balls(List.of(0, 1, 0));
+        isBallOfIndex(1);
     }
 
     @Test
-    @DisplayName("1번째 자리 낫싱")
-    void Nothing() {
-        player = new Balls(List.of(0, 0, 0));
-        baseBall.judgeBallOfIndex(com, player, 0);
+    @DisplayName("2번째 자리 스트라이크")
+    void oneStrike_2() {
+        player = new Balls(List.of(0, 2, 0));
+        isStrikeOfIndex(1);
+    }
+    @Test
+    @DisplayName("3번째 자리 볼")
+    void oneBall_3() {
+        player = new Balls(List.of(0, 0, 1));
+        isBallOfIndex(2);
+    }
+
+    @Test
+    @DisplayName("3번째 자리 스트라이크")
+    void oneStrike_3() {
+        player = new Balls(List.of(0, 0, 3));
+        isStrikeOfIndex(2);
+    }
+
+    private void isBallOfIndex(int idx) {
+        baseBall.judgeBallOfIndex(com, player, idx);
+        assertThat(baseBall.getBall()).isEqualTo(1);
         assertThat(baseBall.getStrike()).isEqualTo(0);
+        assertThat(baseBall.isNothing()).isFalse();
+
+    }
+
+    private void isStrikeOfIndex(int idx) {
+        baseBall.judgeBallOfIndex(com, player, idx);
+        assertThat(baseBall.getStrike()).isEqualTo(1);
         assertThat(baseBall.getBall()).isEqualTo(0);
-        assertThat(baseBall.isNothing()).isTrue();
+        assertThat(baseBall.isNothing()).isFalse();
     }
 }

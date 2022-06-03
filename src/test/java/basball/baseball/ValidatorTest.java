ec66e48d21fe8711d165534a2c0d1d02f5f13497
sukeun <busan-sy@naver.com>
2022-06-03 16:32:32 +0900

    test : hasThreeNumber 테스트 추가 및 중복 제거

--- src/test/java/basball/baseball/ValidatorTest.java
+++ src/test/java/basball/baseball/ValidatorTest.java
@@ -11,11 +11,11 @@ class ValidatorTest {
     @Test
     @DisplayName("게임 종료 후 값이 숫자가 아닌경우")
     void validateAnswerTest_1() {
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("a"));
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("test"));
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("-"));
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("a1"));
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("a2"));
+        validateWrongAnswer("a");
+        validateWrongAnswer("test");
+        validateWrongAnswer("-");
+        validateWrongAnswer("a1");
+        validateWrongAnswer("a2");
     }
 
 
@@ -29,28 +29,74 @@ class ValidatorTest {
     @Test
     @DisplayName("게임 종료 후 값이 1 또는 2가 아닌경우")
     void validateAnswerTest_3() {
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("3"));
-        assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer("-1"));
+        validateWrongAnswer("3");
+        validateWrongAnswer("-1");
+    }
+
+    private IllegalArgumentException validateWrongAnswer(String answer) {
+        return assertThrows(IllegalArgumentException.class, () -> Validator.validateAnswer(answer));
     }
 
     @Test
     @DisplayName("1,2 번째 숫자가 동일")
     void isDifferentNumbersTest_1() {
-        int[] numbers = {1, 1, 2};
-        assertThrows(IllegalArgumentException.class, () -> Validator.isDifferentNumbers(numbers));
+        validateSameNumber(1, 2, 1);
     }
 
     @Test
     @DisplayName("2,3 번째 숫자가 동일")
     void isDifferentNumbersTest_2() {
-        int[] numbers = {2, 1, 1};
-        assertThrows(IllegalArgumentException.class, () -> Validator.isDifferentNumbers(numbers));
+        validateSameNumber(2, 1, 1);
     }
 
     @Test
     @DisplayName("1,3 번째 숫자가 동일")
     void isDifferentNumbersTest_3() {
-        int[] numbers = {2, 1, 2};
+        validateSameNumber(2, 1, 2);
+    }
+
+    private void validateSameNumber(int n1, int n2, int n3) {
+        int[] numbers = {n1, n2, n3};
         assertThrows(IllegalArgumentException.class, () -> Validator.isDifferentNumbers(numbers));
+    }
+
+    @Test
+    @DisplayName("3개의 1~9 숫자 만 입력")
+    void hasThreeNumberTest_1() {
+        String number = "231";
+        Validator.hasThreeNumber(number);
+    }
+
+    @Test
+    @DisplayName("0 을 포함한 숫자 입력")
+    void hasThreeNumberTest_2() {
+        validateWrongNumber("230");
+    }
+
+    @Test
+    @DisplayName("숫자 3개 미만 입력")
+    void hasThreeNumberTest_3() {
+        validateWrongNumber("23");
+    }
+    @Test
+    @DisplayName("숫자 4개 이상 입력")
+    void hasThreeNumberTest_4() {
+        validateWrongNumber(" 2314");
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 문자 입력")
+    void hasThreeNumberTest_5() {
+        validateWrongNumber(" 23a");
+    }
+
+    @Test
+    @DisplayName("입력값 없음")
+    void hasThreeNumberTest_6() {
+        validateWrongNumber("");
+    }
+
+    private void validateWrongNumber(String number) {
+        assertThrows(IllegalArgumentException.class, () -> Validator.hasThreeNumber(number));
     }
 }
